2cab6a954d06d1cf9b76338cdb265bd6f8d03408
gw31415 <git@amas.dev>
2022-02-16 18:35:30 +0900

    escape code mode

--- src/md2html.rs
+++ src/md2html.rs
@@ -1,4 +1,4 @@
-use pulldown_cmark::{html, Event, Options, Parser};
+use pulldown_cmark::{html, Event, Options, Parser, Tag};
 use rand::Rng;
 use regex::{Captures, Regex};
 
@@ -29,6 +29,7 @@ pub struct MetaData {
 struct Refugee {
     label: String,
     processed: String,
+    source: String,
 }
 
 impl Converter {
@@ -48,30 +49,62 @@ impl Converter {
         let mut shelter: Vec<Refugee> = Default::default();
         for rule in &self.bypass_rules {
             let (reg, replacer) = rule;
-            markdown = reg.replace_all(&markdown, |caps: &Captures| {
-                let label = format!("{:>016x}", rng.gen::<u64>());
-                let processed = replacer(caps);
-                shelter.push(Refugee {
-                    label: label.clone(),
-                    processed,
-                });
-                label
-            }).to_string();
+            markdown = reg
+                .replace_all(&markdown, |caps: &Captures| {
+                    let label = format!("{:>016x}", rng.gen::<u64>());
+                    let processed = replacer(caps);
+                    shelter.push(Refugee {
+                        label: label.clone(),
+                        processed,
+                        source: caps[0].to_string(),
+                    });
+                    label
+                })
+                .to_string();
         }
 
         // parse markdown (= body)
+        let mut tag_stack: Vec<Tag> = Default::default();
         let parser =
             Parser::new_ext(&markdown, self.pulldown_cmark_options).map(|event| match event {
                 Event::Html(html) => Event::Text(html),
+                Event::Start(tag) => {
+                    tag_stack.push(tag.clone());
+                    Event::Start(tag)
+                }
+                Event::End(tag) => {
+                    tag_stack.pop();
+                    Event::End(tag)
+                }
+                Event::Text(mut text) => {
+                    if let Some(Tag::CodeBlock(_)) = tag_stack.last() {
+                        for refugee in &shelter {
+                            let Refugee { label, source, .. } = &refugee;
+                            text = text.replace(label, &source).into();
+                        }
+                        Event::Text(text)
+                    } else {
+                        for refugee in &shelter {
+                            let Refugee {
+                                label, processed, ..
+                            } = &refugee;
+                            text = text.replace(label, &processed).into();
+                        }
+                        Event::Html(text)
+                    }
+                }
+                Event::Code(mut code) => {
+                    for refugee in &shelter {
+                        let Refugee { label, source, .. } = &refugee;
+                        code = code.replace(label, &source).into();
+                    }
+                    Event::Code(code)
+                }
                 _ => event,
             });
 
         let mut body = String::new();
         html::push_html(&mut body, parser);
-        for refugee in shelter {
-            let Refugee { label, processed } = &refugee;
-            body = body.replace(label, processed);
-        }
         (self.template)(body, metadata)
     }
 }
